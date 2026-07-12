0d89d9d05584e957e7b5659face824d1b62f503a
Tsakiridis Sotiris <artibetgr@gmail.com>
2026-07-12 20:55:14 +0300

    Update vide build config

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -7,6 +7,7 @@ import '../css/app.css';
 
 import { createInertiaApp } from '@inertiajs/react'
 import { createRoot } from 'react-dom/client';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { router } from '@inertiajs/react';
 import NProgress from 'nprogress';
 
@@ -14,10 +15,7 @@ const appName = import.meta.env.VITE_APP_NAME || 'Aneser';
 
 createInertiaApp({
   title: (title) => `${title} - ${appName}`,
-  resolve: name => {
-    const pages = import.meta.glob('./Pages/**/*.jsx', { eager: true })
-    return pages[`./Pages/${name}.jsx`].default
-  },
+  resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
   setup({ el, App, props }) {
     createRoot(el).render(<App {...props} />)
   },
